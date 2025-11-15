ef4702a1e147c3e21d5411e86ea9be3169ae4f07
Arif Renggy <arifrenggy00@gmail.com>
2025-11-15 17:41:54 +0800

    update config/database.php

--- config/database.php
+++ config/database.php
@@ -1,17 +1,27 @@
 <?php
 class Database {
-    private $host = "127.0.0.1";
-    private $db_name = "amarcell_db";
-    private $username = "root";
-    private $password = "";
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $port;
     private $conn;
+
+    public function __construct() {
+        // Ambil dari Railway Environment Variables
+        $this->host     = getenv("MYSQLHOST");
+        $this->db_name  = getenv("MYSQLDATABASE");
+        $this->username = getenv("MYSQLUSER");
+        $this->password = getenv("MYSQLPASSWORD");
+        $this->port     = getenv("MYSQLPORT") ?: 3306;
+    }
 
     public function getConnection() {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
+            $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->db_name};charset=utf8";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
